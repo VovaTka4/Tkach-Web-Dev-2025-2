118337098e22c6c579a42c4ed30016e40d55ddbf
VovaTka4 <EpikBulldogg@mail.ru>
2025-03-27 11:09:24 +0300

    root_app added, for multi-app

---
+++ app.py
@@ -0,0 +1,11 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from Lab1.app import app as lab1_app
+from Lab2.app import app as lab2_app
+from root_app.app import app as root_app
+
+app = DispatcherMiddleware(root_app, {
+    '/lab1': lab1_app,
+    '/lab2': lab2_app
+})
+
+application = app
