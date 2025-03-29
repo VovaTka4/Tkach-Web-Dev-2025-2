fd4bef290751f4fcf38579713bf590c2314fbeeb
VovaTka4 <EpikBulldogg@mail.ru>
2025-03-30 00:14:51 +0300

    all tests except redirect to secret after login are done

--- Lab3/app/tests/test_lab3.py
+++ Lab3/app/tests/test_lab3.py
@@ -5,3 +5,133 @@ def test_visits_counter(client):
     assert response.status_code == 200
     page_content = response.get_data(as_text=True)
     assert "1" in page_content
+    client.get("/visitscounter")
+    client.get("/visitscounter")
+    response = client.get("/visitscounter")
+    page_content = response.get_data(as_text=True)
+    assert "4" in page_content
+    
+def test_visits_counter_for_user(client):
+    client.get("/visitscounter")
+    client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty'
+    }, follow_redirects=True)
+    response = client.get("/visitscounter")
+    assert response.status_code == 200
+    page_content = response.get_data(as_text=True)
+    assert "1" in page_content
+    assert "2" in page_content
+    client.get("/visitscounter")
+    client.get("/visitscounter")
+    response = client.get("/visitscounter")
+    page_content = response.get_data(as_text=True)
+    assert "4" in page_content
+    assert "5" in page_content
+    
+def test_navbar_logged_in_logged_out(client):
+    response = client.get('/')
+    page_content = response.get_data(as_text=True)
+    assert "Вход" in page_content
+    assert "Счётчик посещений" in page_content
+    assert "Выход" not in page_content
+    assert "Секретная страница" not in page_content
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty'
+    }, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert "Вход" not in page_content
+    assert "Счётчик посещений" in page_content
+    assert "Выход" in page_content
+    assert "Секретная страница" in page_content
+
+
+    
+def test_login_redirect_after_authentication(client):
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty'
+    }, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert 'Вы успешно аутентифицированы!' in page_content
+    assert 'Добро пожаловать, user!' in page_content
+    assert response.request.path == '/'
+    # print(page_content)
+    assert '/logout' in page_content
+    
+def test_login_with_wrong_data(client):
+    response = client.post('/login', data={
+        'username': 'user2',
+        'password': 'qwert'
+    }, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert 'Введены неверные данные!' in page_content
+    
+def test_login_with_uncompleted_data(client):
+    response = client.post('/login', data={
+        'username': 'user2',
+    }, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert 'Необходимо заполнить все поля!' in page_content
+    
+def test_login_with_no_data(client):
+    response = client.post('/login', data={}, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert 'Необходимо заполнить все поля!' in page_content
+    
+def test_secret_page_is_hidden(client):
+    response = client.get('/')
+    page_content = response.get_data(as_text=True)
+    assert '/secretpage' not in page_content
+    
+def test_secret_page_is_not_hidden(client):
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty'
+    }, follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert '/secretpage' in page_content
+    
+def test_unauthorized_user_secret_redirect(client):
+    response = client.get('/secretpage', follow_redirects=True)
+    page_content = response.get_data(as_text=True)
+    assert response.request.path == '/login'
+    assert 'Войдите в аккаунт для доступа к материалам сайта)' in page_content
+    
+def test_remember_me_token_setted(client):
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty',
+        'remember_me': 'on'
+    }, follow_redirects=True)
+
+    assert 'Вы успешно аутентифицированы!' in response.get_data(as_text=True)
+    # print("ЗАГОЛОВКИИИИ::::::", response.headers)
+    
+    assert response.request.cookies.get('remember_token', None)
+    # print("КУКИЧ::::::::::::::::: ", response.request.cookies.get('remember_token', None))
+    
+def test_remember_me_token_not_setted(client):
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty',
+        'remember_me': 'off'
+    }, follow_redirects=True)
+
+    assert 'Вы успешно аутентифицированы!' in response.get_data(as_text=True)
+    
+    assert not response.request.cookies.get('remember_token', None)
+    
+def test_unauthorized_user_secret_redirect_after_login(client):
+    response = client.get('/secretpage', follow_redirects=True)
+    assert 'next=/secretpage' in response.request.url
+
+    response = client.post('/login', data={
+        'username': 'user',
+        'password': 'qwerty'
+    }, follow_redirects=True)
+    
+    assert response.request.path == '/secretpage'
+    page_content = response.get_data(as_text=True)
+    assert 'Самый страшный секрет - это читать далее...' in page_content
