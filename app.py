45efe965263df3344fb3750d85a6cfd0891082bb
VovaTka4 <EpikBulldogg@mail.ru>
2025-04-20 18:42:37 +0300

    root app changed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from Lab1.app.app import app as lab1_app
 from Lab2.app.app import app as lab2_app
 from Lab3.app.app import app as lab3_app
-from Lab4.app.app import app as lab4_app
+from Lab4.app.__init__ import app as lab4_app
 from root_app.app import app as root_app
 
 app = DispatcherMiddleware(root_app, {
