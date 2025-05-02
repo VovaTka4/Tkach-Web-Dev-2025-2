c4d4901e27138f29e0f0095f604fbcfdf1ec6af6
VovaTka4 <EpikBulldogg@mail.ru>
2025-05-02 20:10:09 +0300

    user dir

--- Lab5/app/decorators/check_rights.py
+++ Lab5/app/decorators/check_rights.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import redirect, url_for, flash, g
 from flask_login import current_user
+import logging
 
 from ..repositories.role_repository import RoleRepository
 from ..db import db
@@ -14,7 +15,7 @@ def check_rights(required_permission):
             user_role = role_repository.get_by_id(current_user.role_id).name
             g.has_permission = (user_role == required_permission)
             user_id = kwargs.get("user_id")
-            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + dir(current_user))
+            logging.warning(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> { dir(current_user) }")
             if (not g.has_permission) or (user_role != required_permission and current_user.id == user_id):
                 flash("У вас недостаточно прав для данного действия.", "danger")
                 return redirect(url_for("index"))
