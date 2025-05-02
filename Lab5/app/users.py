29439e418532e2d56612f6353db9f4872a457017
VovaTka4 <EpikBulldogg@mail.ru>
2025-05-02 20:59:31 +0300

    debugging

--- Lab5/app/users.py
+++ Lab5/app/users.py
@@ -15,6 +15,7 @@ role_repository = RoleRepository(db)
 bp = Blueprint('users', __name__, url_prefix='/users')
 
 @bp.route('/')
+@check_rights('admin')
 def index():
     return render_template('users/index.html', users=user_repository.all())
 
