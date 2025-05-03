d91a39b28f8444044449cb1679e2a4d17d76dd0a
VovaTka4 <EpikBulldogg@mail.ru>
2025-05-03 20:52:22 +0300

    debugging

--- Lab5/app/decorators/check_rights.py
+++ Lab5/app/decorators/check_rights.py
@@ -22,17 +22,19 @@ def check_rights(required_permission):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):   
-                    
+            
+            if not current_user.is_authenticated:
+                return func(*args, **kwargs)
+        
             user_id = kwargs.get('user_id')
             
             g.is_admin = has_rights(required_permission)
-            g.has_rights =  current_user.is_authenticated and (user_id == current_user.id)
+            g.has_rights =  (user_id == current_user.id)
             
             if g.has_rights or g.is_admin:
                 return func(*args, **kwargs)
             
-            if current_user.is_authenticated:
-                flash('У вас недостаточно прав для доступа к данной странице! USERID:' + str(user_id) + " CURRENTUSERID: " + str(current_user.id), 'warning')
+            flash('У вас недостаточно прав для доступа к данной странице! USERID:' + str(user_id) + " CURRENTUSERID: " + str(current_user.id), 'warning')
             
             return redirect(url_for('users.index'))
         
