b573dc1c7241119fc8e0b80a21ce8adb0b8572ca
VovaTka4 <EpikBulldogg@mail.ru>
2025-05-03 23:02:32 +0300

    debugging

--- Lab5/app/static/user_delete_modal.js
+++ Lab5/app/static/user_delete_modal.js
@@ -2,7 +2,15 @@
 
 function modalShown(event) {
     let button = event.relatedTarget;
+    let userId = button.dataset.userId;
     let newUrl =  button.dataset.deleteUrl;
+    let form = document.getElementById('deleteModalForm');
+    let row = button.closest('tr');
+    let lastName = row.querySelector('td:nth-child(3)').textContent;
+    let Name = row.querySelector('td:nth-child(4)').textContent;
+    let middleName = row.querySelector('td:nth-child(5)').textContent;
+    let modalUserName = document.getElementById('modalUserName');
+    modalUserName.textContent = lastName + " " + Name + " " + middleName;
     form.action = newUrl;
 }
 
